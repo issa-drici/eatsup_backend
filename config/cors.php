487fb22497409a4e1039c57900794a0ba60511b2
issa-drici <driciissa76@gmail.com>
2025-01-04 08:56:22 +0100

    add cors origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://v2.eatsup.fr', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
