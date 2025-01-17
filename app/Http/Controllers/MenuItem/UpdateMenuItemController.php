d9c08c65bf64eb2059bff2db25ba44db30af86ae
issa-drici <driciissa76@gmail.com>
2025-01-17 14:16:42 +0100

    ini restrictions

--- app/Http/Controllers/MenuItem/UpdateMenuItemController.php
+++ app/Http/Controllers/MenuItem/UpdateMenuItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\MenuItem;
 use App\Application\Usecases\MenuItem\UpdateMenuItemUsecase;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class UpdateMenuItemController extends Controller
 {
@@ -14,30 +15,45 @@ class UpdateMenuItemController extends Controller
 
     public function __invoke(string $menuItemId, Request $request)
     {
-        $data = $request->validate([
-            'name'        => ['sometimes', function ($attribute, $value, $fail) {
-                if ($value && !is_array(json_decode($value, true))) {
-                    $fail('Le champ name doit être un JSON valide.');
-                }
-            }],
-            'description' => ['nullable', function ($attribute, $value, $fail) {
-                if ($value && !is_array(json_decode($value, true))) {
-                    $fail('Le champ description doit être un JSON valide.');
-                }
-            }],
-            'price'      => 'sometimes|numeric',
-            'allergens'  => 'nullable|array',
-            'images'     => 'nullable|array',
-            'images.*'   => [
-                'file',
-                'image',
-                'max:30720',
-            ],
-            'remove_images' => 'nullable|array',
-            'remove_images.*' => 'string|uuid',
-            'sort_order' => 'nullable|integer',
-            'is_active'  => 'nullable|accepted|in:1,true,on,yes'
-        ]);
+        Log::info('Taille maximale de upload_max_filesize: ' . ini_get('upload_max_filesize'));
+        Log::info('Taille maximale de post_max_size: ' . ini_get('post_max_size'));
+        Log::info('Fichiers reçus:', $request->allFiles());
+        
+        try {
+            $data = $request->validate([
+                'name'        => ['sometimes', function ($attribute, $value, $fail) {
+                    if ($value && !is_array(json_decode($value, true))) {
+                        $fail('Le champ name doit être un JSON valide.');
+                    }
+                }],
+                'description' => ['nullable', function ($attribute, $value, $fail) { 
+                    if ($value && !is_array(json_decode($value, true))) {
+                        $fail('Le champ description doit être un JSON valide.');
+                    }
+                }],
+                'price'      => 'sometimes|numeric',
+                'allergens'  => 'nullable|array',
+                'images'     => 'nullable|array',
+                'images.*'   => [
+                    'file',
+                    'image',
+                    'max:92160',
+                ],
+                'remove_images' => 'nullable|array',
+                'remove_images.*' => 'string|uuid',
+                'sort_order' => 'nullable|integer',
+                'is_active'  => 'nullable|accepted|in:1,true,on,yes'
+            ]);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            Log::error('Erreur de validation:', [
+                'errors' => $e->errors(),
+                'file_info' => $request->file('images') ? [
+                    'size' => $request->file('images')[0]->getSize(),
+                    'mime' => $request->file('images')[0]->getMimeType(),
+                ] : null
+            ]);
+            throw $e;
+        }
 
         // Décoder les champs JSON
         if (isset($data['name'])) {
